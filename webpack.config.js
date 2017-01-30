fcd3860656a51f4cb1ed5b042338597d73cbd894
Nicholas Eli <nick@nicholasbydesign.com>
2017-01-30 07:59:48 -0800

    UPDATE: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
-var debug = process.env.NODE_ENV !== "production";
+var path = require('path');
+var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 
+
 module.exports = {
-  context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./babel/script.js",
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query:{
-          presets: ['react', 'es2015', 'stage-3'],
-          plugins: ['transform-react-jsx']
-        }
-      }
-    ]
-  },
-  output: {
-    path: __dirname + "/js",
-    filename: "script.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+    context: __dirname,
+    devtool: debug ? 'inline-sourcemap' : null,
+    entry: './babel/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'script.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /(node_modules|bower_components)/,
+            include: [path.resolve(__dirname, 'babel/script.js')],
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-3'],
+                plugins: ['transform-react-jsx']
+            }
+        }, {
+            test: /\.scss$/,
+            include: [path.resolve(__dirname, 'sass/style.scss')],
+            loaders: ['style-loader' ,'css-loader' ,'sass-loader']
+        }]
+    },
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, './sass')]
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: false,
+            sourcemap: false
+        }),
+    ],
 };
